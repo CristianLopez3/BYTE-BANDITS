9635ce6960fb7fb47233b18fed040a3ce9374f5d
Cristian <cristian.c.lopez.m@gmail.com>
2023-09-25 17:07:09 -0500

    test controller run

---
+++ src/main/java/com/sena/senasoft/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.sena.senasoft.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+class TestController {
+
+    @GetMapping
+    public String test(){
+        return "test";
+    }
+
+
+
+}
