7c2f3c055f0d27b09ff4e471e731aa4313f044e9
Ndobito <jcuellarmenza@gmail.com>
2023-09-27 09:53:20 -0500

    Header Complete

---
+++ view/footer.php
@@ -0,0 +1,10 @@
+
+        <footer></footer>
+    <!-- Document -->
+    <?php echo (isset($$js)) ? $js : "" ?>
+    <!-- Font Awesome - Icons -->
+    <script src="https://kit.fontawesome.com/7fa9974a48.js" crossorigin="anonymous"></script>
+    <!-- Boostrap Link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
--- view/index/index.php
+++ view/index/index.php
@@ -1,17 +1,38 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php echo (isset($style)) ? $style : ""; ?>
-</head>
-<body>
-    <div class="container">
-    <h1>Hola Mundo</h1>
-    <p id="message"></p>
-    </div>
 
-    <script src="assets/js/index.js"></script>
-</body>
-</html>
+<section id="hero">
+            <div class="container">
+                <div class="row">
+                    <article class="col-lg-6 ">
+                        <div id="carouselHeroIndicators" class="carousel slide" data-bs-ride="true">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselHeroIndicators" data-bs-slide-to="0"
+                                    class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselHeroIndicators" data-bs-slide-to="1"
+                                    aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselHeroIndicators" data-bs-slide-to="2"
+                                    aria-label="Slide 3"></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <h3>Comparte y encuentra soluciones en la comunidad de tu preferencia</h3>
+                                    <p>Aquí, la búsqueda de respuestas a tus desafíos de programación nunca ha sido tan sencilla. ¿Estás atascado en tu código? ¿Necesitas una solución rápida? Has llegado al lugar adecuado. Nuestro compromiso es ofrecerte una comunidad de expertos y entusiastas de la programación que están listos para ayudarte a superar cualquier obstáculo.</p>
+                                </div>
+                                <div class="carousel-item">
+                                    <h3>Adquiere conocimiento de manera autodidacta</h3>
+                                    <p>Explora nuestros foros de programación, donde la comunidad comparte conocimientos, resuelve problemas y colabora en proyectos emocionantes. Ya sea que estés lidiando con un error misterioso o buscando formas innovadoras de mejorar tu código, estamos aquí para apoyarte en cada paso del camino.</p>
+                                </div>
+                                <div class="carousel-item">
+                                    <h3>Encuentra las soluciones en menor tiempo</h3>
+                                    <p>No pierdas más tiempo buscando en vano. En Byte Forum, encontrarás soluciones concretas para tus desafíos de codificación. Únete a nuestra comunidad hoy mismo y da vida a tus proyectos de programación como nunca antes lo habías hecho.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </article>
+                    <article class="col-lg-6 cont-info">
+                        <img src="assets/images/hero.png" alt="">
+                    </article>
+                </div>
+            </div>
+        </section>
+    </header>
