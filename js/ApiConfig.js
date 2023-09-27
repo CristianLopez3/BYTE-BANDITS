4113d1392b281b1aae16755c333aa2903c79109a
Ndobito <jcuellarmenza@gmail.com>
2023-09-27 15:28:17 -0500

    Merge

---
+++ js/ApiConfig.js
@@ -0,0 +1,16 @@
+// Spring boot URL 
+export const apiUrl  = "http://localhost:9999";
+
+// Header
+export const commonHeaders = {
+  'Content-Type': 'application/json'
+};
+
+
+// Function to manage errors
+export function handleResponse(response) {
+  if (!response.ok) {
+    throw new Error(`Error HTTP: ${response.status}`);
+  }
+  return response.json();
+}
