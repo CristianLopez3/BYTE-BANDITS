dc98566b1914cab57c0aff3c9322f47d7cce4ac6
Cristian-Lopez <126030346+CristianLopez3@users.noreply.github.com>
2023-09-27 12:34:25 -0500

    add cors configuration to comprobe data

---
+++ src/main/java/com/sena/senasoft/infra/config/Cors.java
@@ -0,0 +1,28 @@
+package com.sena.senasoft.infra.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class Cors {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        // Configure the rules for the cors filter
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // Permite solicitudes de cualquier origen
+        corsConfiguration.addAllowedMethod("*"); // Permite cualquier método (GET, POST, PUT, DELETE, etc.)
+        corsConfiguration.addAllowedHeader("*"); // Permite cualquier encabezado
+
+        // Apply the configuration for the all routes
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        // return the CORS filter configured
+        return new CorsFilter(source);
+    }
+}
+
